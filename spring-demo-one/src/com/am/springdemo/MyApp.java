ddac03f25a187da999c531cfbda08b557e9d440d
ancuta <ancuta.maria@gmail.com>
2021-05-09 13:10:25 +0300

    spring-demo-one added IoC with xml. First Spring config! iuhuu + changed logging

--- spring-demo-one/src/com/am/springdemo/MyApp.java
+++ spring-demo-one/src/com/am/springdemo/MyApp.java
@@ -4,11 +4,11 @@ public class MyApp {
 
 	public static void main(String[] args) {
 
-		Coach theCoach = new BaseballCoach();
-		System.out.println(theCoach.getDailyWorkout());
+//		Coach theCoach = new BaseballCoach();
+//		System.out.println(theCoach.getDailyWorkout());
 		
-		Coach theCoach2 = new TrackCoach();
-		System.out.println(theCoach2.getDailyWorkout());
+		Coach theCoach = new TrackCoach();
+		System.out.println(theCoach.getDailyWorkout());
 	
 	}
 
